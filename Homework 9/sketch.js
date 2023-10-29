ae897dfaace9aee0fb4853620a1af2411247c5f5
illuminatedmovement <143444365+illuminatedmovement@users.noreply.github.com>
2023-10-29 16:36:18 -0600

    homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+    createCanvas(500,600);
+  }
+  
+  function draw() {
+    background(120,55,120);
+    fill(290,45,39);
+    circle(250,75,125);
+    fill(12);
+    ellipse(250,100,30,10);
+    fill(0,0,110);
+    ellipse(230,65,10,5);
+    ellipse(270,65,10,5);
+    fill(150,25,0);
+    ellipse(250,125,30,50);
+    fill(55);
+    ellipse(250,200,50,80);
+  }
+  function draw()
+{
+    background(120,45,78);
+    textSize(22)
+    text("My Basic Structure", 10,80);
+
+    fill(23, 204, 35);
+    circle(350,100,175);
+   
+    strokeWeight(12);
+    fill(0,255,255);
+    point(300,75);
+    point(385,75);
+
+    point(345,90);
+
+    strokeWeight(5);
+    ellipse(345, 135, 50, 25)
+
+    line(350,45,225,70);
+    line(350,45,425,60);
+    
+    fill(120);
+    rect(300,185,100,150);
+    
+    fill(10, 60, 120);
+    rect(400,195,50,10);
+    
+    rect(250,195,50,10);
+    
+    rect(300,335,10,50);
+    
+    rect(390,335,10,50);
+
+    triangle(350,2,250,45,440,45);
+    
+    fill(180);
+    textSize(16);
+    text("Jeremy Jeffay",20,500 );
+
+
+}
