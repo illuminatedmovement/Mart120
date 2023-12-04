207743b5c053b74147a7447f66de5afae4f1ea87
illuminatedmovement <143444365+illuminatedmovement@users.noreply.github.com>
2023-12-04 11:49:45 -0700

    HW 14

---
+++ Homework 14/sketchHW14.js
@@ -0,0 +1,208 @@
+
+var x = 50;
+var y = 50;
+var diameter = 25;
+
+var playerX = 20;
+var playerY = 20;
+var mousex;
+var mousey;
+
+var shapeX = 20;
+var shapeY = 40;
+
+var shapeXs = [];
+var shapeYs = [];
+var diameters = [];
+
+var shapeXSpeeds = [];
+var shapeYSpeeds = [];
+
+function setup()
+{
+    createCanvas(800,600);
+    for (var i = 0; i < 50; i++) 
+    {
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 7)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 7)) + 1);
+        shapeXs[i] = getRandomNumber(800);
+        shapeYs[i] = getRandomNumber(600);
+        diameters[i] = getRandomNumber(30);
+    }
+}
+
+function draw()
+{
+    background(100,20,200);
+    
+    createPlayer();
+    
+    playerMove();
+
+    mouseDrawn();
+
+    createExit();
+
+    createObstacle1();
+
+    moveObstacle1();
+
+    createObstacles();
+
+    displayWin();
+
+    createBorders();
+}
+
+
+
+function createPlayer()
+{
+    fill(20,200,200);//player
+    ellipse(playerX,playerY,10,20);
+}
+function playerMove()
+{
+    if(keyIsDown(68))
+    {playerX+=6;}
+    if(keyIsDown(65))
+    {playerX-=6;}
+    if(keyIsDown(87))
+    {playerY-=6;}
+    if(keyIsDown(83))
+    {playerY+=6;}
+}
+function mouseDrawn()
+{
+    fill(10,10,220);//mouse drawn shape
+    circle(mousex,mousey,60); 
+}
+function createObstacle1()
+{
+    fill(124,17,79);//obstacle1
+    circle(x,y,diameter);
+}
+function moveObstacle1()
+{
+    if (x > 800) {//obstacle1 movement
+        x = 0;
+    }
+    else if (x > 200) {
+        x += 5;
+    }
+    else if (x <= 300) {
+        x += 10;
+    } 
+    
+    if (y > 300) {
+        y = 50;
+    }
+    else if (y > 200) {
+        y += 1;
+    } 
+    else if (y <= 300) {
+        y += 3;
+    }
+
+    if (diameter > 150) {
+        diameter = 25;
+    }
+    else if (diameter > 50) {
+        diameter += 4;
+    } 
+    else if (diameter <= 100) {
+        diameter += 1;
+    } 
+}
+function createObstacles()
+{
+    
+    fill(20,225,20);
+    //obstacles
+    for (var i = 0; i < shapeXs.length; i++) {
+        ellipse(shapeXs[i], shapeYs[i], diameters[i]);
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+
+
+
+        //obstacles movement
+        if (shapeXs[i] < 0) 
+        {
+            shapeXs[i] = 650;
+        }
+        else if (shapeXs[i] > 200) {
+            shapeXs[i] -= 5;
+        }
+        else if (shapeXs[i] <= 300) {
+            shapeXs[i] += 1;
+        } 
+        
+        if (shapeYs[i] > 600 || shapeYs[i] < 200) {
+            shapeYs[i] = random(600);
+        }
+        else if (shapeYs[i] > 200) {
+            shapeYs[i] += 1;
+        } 
+        else if (shapeYs[i] <= 300) {
+            shapeYs[i] += 3;
+        } 
+        // check boundary
+        if (shapeXs[i] > 800) {
+            shapeXs[i] = 0;
+        }
+        if (shapeXs[i] < 0) {
+            shapeXs[i] = 800;
+        }
+        if (shapeYs[i] > 600) {
+            shapeYs[i] = 0;
+        }
+        if (shapeYs[i] < 0) {
+            shapeYs[i] = 600;
+        }
+    }
+    
+
+}
+
+
+
+
+
+function createExit()
+{
+    fill(255,255,255);
+    circle(795,595,30);
+}
+function displayWin()
+{
+    fill(0,200,190);
+  textSize(28);
+  text("EXIT! --->", 650, 550, width-30, height-40);
+  
+  if(playerX >= 790 & playerY >= 590)
+  {
+    fill(2,2,220);
+    textSize(40);
+    text("Winner Winner Winner!!!", 250,300);
+  }
+}
+function createBorders()
+{
+    fill(180,20,60);
+    stroke(5);
+    line(1,1,799,1);
+    line(799,1,799,599);
+    line(799,599,1,599);
+    line(1,599,1,1);
+}
+function mouseClicked() 
+{  
+  mousex = mouseX;
+  mousey = mouseY;
+
+}
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number) + 10;
+}
